03437a06bc75492453dcd8d32519a474d69dd1ca
Buramuh <buramuh@gmail.com>
2023-11-28 15:37:59 +0100

    Create tracks table

--- src/test/resources/db-create.sql
+++ src/test/resources/db-create.sql
@@ -21,3 +21,14 @@ create table stations
     foreign key (observable_id) references observables(id),
     unique key (name)
 );
+
+create table tracks
+(
+    observable_id int,
+    station1 varchar(255),
+    station2 varchar(255),
+    primary key (observable_id),
+    foreign key (station1) references stations(name),
+    foreign key (station2) references stations(name),
+    unique key (station1, station2)
+);
