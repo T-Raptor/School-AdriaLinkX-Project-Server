08f338a21b80d4800138dc9ab9e5b867c848bd27
Buramuh <buramuh@gmail.com>
2023-11-28 15:05:50 +0100

    Create stations table

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -3,3 +3,19 @@ create table quotes
     id    int auto_increment,
     quote varchar(255)
 );
+
+create table observables
+(
+    id  int auto_increment,
+    primary key (id)
+);
+
+create table stations
+(
+    observable_id int,
+    name varchar(255),
+    latitude double,
+    longitude double,
+    primary key (observable_id),
+    foreign key (observable_id) references observables(id)
+);
