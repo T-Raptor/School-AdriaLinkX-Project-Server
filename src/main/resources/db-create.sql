1eb89d4d07132e50a8868405924646e4a81868b9
Buramuh <buramuh@gmail.com>
2023-11-29 10:21:50 +0100

    Make tracks(observable_id) a foreign key

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -27,6 +27,7 @@ create table tracks
     station1 varchar(255),
     station2 varchar(255),
     primary key (observable_id),
+    foreign key (observable_id) references observables(id),
     foreign key (station1) references stations(name),
     foreign key (station2) references stations(name),
     unique key (station1, station2)
