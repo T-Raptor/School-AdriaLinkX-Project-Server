4a43f1293dad5f244767819b5d98de4369367be4
Vanhoorne Bram <bram.vanhoorne@student.howest.be>
2023-12-18 00:08:14 +0100

    Create notification repository interface

---
+++ src/main/java/be/howest/ti/adria/logic/data/NotificationRepository.java
@@ -0,0 +1,11 @@
+package be.howest.ti.adria.logic.data;
+
+import be.howest.ti.adria.logic.domain.Notification;
+
+import java.util.List;
+
+public interface NotificationRepository {
+    List<Notification> getNotifications();
+    Notification insertNotification(int event, String company);
+    Notification updateNotification(boolean read);
+}
