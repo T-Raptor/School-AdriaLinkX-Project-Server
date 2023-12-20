176fab454c84e184cbf53ec0c5f8a68ed1f6492a
Vanhoorne Bram <bram.vanhoorne@student.howest.be>
2023-12-20 10:37:03 +0100

    Remove demo reservations, events, notifications and shuttles

--- src/main/resources/db-populate.sql
+++ src/main/resources/db-populate.sql
@@ -15,32 +15,3 @@ INSERT INTO tracks VALUES (SCOPE_IDENTITY(), 2, 3);
 
 INSERT INTO observables VALUES (6);
 INSERT INTO tracks VALUES (SCOPE_IDENTITY(), 1, 3);
-
-INSERT INTO observables VALUES (7);
-INSERT INTO reservations VALUES (SCOPE_IDENTITY(), '2022-05-08 14:30:00', '2022-05-08 18:30:00', 'Hoogle');
-INSERT INTO reservation_tracks VALUES (SCOPE_IDENTITY(), 4);
-INSERT INTO reservation_tracks VALUES (SCOPE_IDENTITY(), 5);
-
-INSERT INTO observables VALUES (8);
-INSERT INTO reservations VALUES (SCOPE_IDENTITY(), '2022-05-12 9:30:00', '2022-05-09 11:30:00', 'Macrosoft');
-INSERT INTO reservation_tracks VALUES (SCOPE_IDENTITY(), 6);
-
-INSERT INTO observables VALUES (9);
-INSERT INTO shuttles VALUES (SCOPE_IDENTITY(), 'AEFF-B217-A4FB');
-
-INSERT INTO observables VALUES (10);
-INSERT INTO shuttles VALUES (SCOPE_IDENTITY(), 'A21F-2936-24AC');
-
-INSERT INTO events (target, moment, class, reason) VALUES (4, '2022-04-12 9:30:10', 'WARN', 'Moose sighted');
-INSERT INTO events (target, moment, class, reason) VALUES (5, '2022-05-12 9:40:09', 'BREAK', 'Giant rock');
-
-INSERT INTO events (target, moment, class, local, latitude, longitude)
-VALUES (9, '2022-05-14 10:30:21', 'MOVE', true, 3.583005176613367, 49.366870654264495);
-
-INSERT INTO events (target, moment, class, local, latitude, longitude)
-VALUES (10, '2022-05-17 10:30:21', 'MOVE', true, 13.583005176613367, 49.366870654264495);
-
-INSERT INTO notifications (event, company) VALUES (1, 'Macrosoft');
-INSERT INTO notifications (event, company) VALUES (2, 'Hoogle');
-INSERT INTO notifications (event, company) VALUES (3, 'Macrosoft');
-INSERT INTO notifications (event, company) VALUES (4, 'Hoogle');
